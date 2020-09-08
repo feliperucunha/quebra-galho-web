cbaedd1e7edc5d4f36b8dcc9771b847e298b618d
Lucas Guedes <69358262+guedev@users.noreply.github.com>
2020-09-08 18:47:13 -0300

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import WorkerList from './pages/WorkerList';
 import WorkerForm from './pages/WorkerForm';
+import AboutUs from './pages/AboutUs';
 
 function Routes() {
-    return (
-        <BrowserRouter>
-            <Route path="/" exact component={Landing} />
-            <Route path="/hire" component={WorkerList} />
-            <Route path="/work" component={WorkerForm} />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Landing} />
+      <Route path="/hire" component={WorkerList} />
+      <Route path="/work" component={WorkerForm} />
+      <Route path="/about-us" component={AboutUs} />
+    </BrowserRouter>
+  );
 }
 
 export default Routes;
